c77be20183863b44b4166154657569c9d897dd3c
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-10-03 21:27:27 +0700

    Refactor Function

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -14,17 +14,19 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	prefix := englishHelloPrefix
-
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case french:
 		prefix = frenchHelloPrefix
 	case spanish:
 		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
