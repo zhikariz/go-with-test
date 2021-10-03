30ec75f4d90f4d70081b8229878cef08dce0ffde
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-10-03 17:09:53 +0700

    Well-Written Failing Test Hello World

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper() // with this func, error line will be reported which is the error line happening, not from this line
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
